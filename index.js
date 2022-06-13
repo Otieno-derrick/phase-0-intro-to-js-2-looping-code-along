0c1910dfb3ca6ab4eb7727bb89ca1ae724dec099
derrique <otieno.derrick@student.moringaschool.com>
2022-06-13 08:10:50 +0300

    commit tree 25dd6616b1430b7bd86d25c5daaf1ca07af754e9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(cards) {
+    // for ( var i = 0; i < cards.length; i++){
+    //  console.log(`Thank you ${cards[i]} for the wonderful suprise gift!`);
+    let i = 0;
+    while (i < 3) {
+    console.log(`Thank you ${cards[i]} for the wonderful suprise gift!`);
+    i++;
+}
+
+return cards;
+
+}
+
+writeCards(cards);
