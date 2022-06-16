8a651bb606aeb89cbe8aa8dc7f2a8302dc445ca3
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-16 15:57:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(newNames, event) {
+  let myCards = []
+    for (let i = 0; i < newNames.length; i++) {
+      myCards.push(`Thank you, ${newNames[i]}, for the wonderful ${event} gift!`);
+    }
+return myCards;
+}
+
+
+
+function countDown(myNumbers) {
+  while (myNumbers > 0 ) {
+    console.log(myNumbers);
+    myNumbers -= 1;
+  }
+  console.log(myNumbers);
+}
